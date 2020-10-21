1c757824a050783e0c13a76267aa3f4ec8129081
publishbot <publishbot@spoiledcat.net>
2020-10-21 21:40:44 +0000

    com.spoiledcat.quick-console v2.0.23+9bc4f2be86

---
+++ Editor/Extensions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright 2018-2019 Andreia Gaita
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SpoiledCat.Extensions
+{
+	public static class Extensions
+	{
+		public static IEnumerable<T> ForEach<T>(this IEnumerable<T> enumerable, Action<T> func)
+		{
+			foreach (var item in enumerable)
+			{
+				func(item);
+				yield return item;
+			}
+		}
+
+		public static string TryGetLocation(this Assembly asm)
+		{
+			try
+			{
+				return asm.Location;
+			}
+			catch
+			{
+				return null;
+			}
+		}
+	}
+}
