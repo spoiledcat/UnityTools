72a55a1efd8047a2673c3ba215eae5b4982c0ba6
Andreia Gaita <shana@spoiledcat.net>
2020-10-21 18:24:11 +0200

    Publishing build branches per package, let's see if this works...

--- scripts/Publish-Branches.ps1
+++ scripts/Publish-Branches.ps1
@@ -11,9 +11,16 @@ if ($Trace) {
 
 . $PSScriptRoot\helpers.ps1 | out-null
 
-$version = & "$($env:USERPROFILE)\.nuget\packages\nerdbank.gitversioning\3.1.91\tools\Get-Version.ps1"
+$version = $env:CloudBuildNumber
+$isPublic = $env:PublicRelease
+
+if (!$version -or $version -eq "") {
+    $versionData = & "$($env:USERPROFILE)\.nuget\packages\nerdbank.gitversioning\3.1.91\tools\Get-Version.ps1"
+    $version = $versionData.CloudBuildNumber
+    $isPublic = $versionData.PublicRelease
+}
+
 $srcdir = Join-Path $rootDirectory 'build\packages'
-$isPublic = $version.PublicRelease
 
 Push-Location $srcdir
 
@@ -24,20 +31,20 @@ try {
             Push-Location $_.Name
             $branch = "packages/$($_.Name)"
             if ($isPublic) {
-                $branch = "$branch/$($version.CloudBuildNumber)"
+                $branch = "$branch/v$($version)"
             } else {
                 $branch = "$branch/latest"
             }
-            $msg = "$($_.Name) v$($version.CloudBuildNumber)"
-            Write-Output "Publishing $branch $($version.CloudBuildNumber)"
+            $msg = "$($_.Name) v$($version)"
+            Write-Output "Publishing branch: $branch ($($version))"
             Invoke-Command -Quiet { & git init . }
             Invoke-Command -Quiet { & git remote add origin git@github.com:spoiledcat/UnityTools }
             Invoke-Command -Quiet { & git fetch origin }
             Invoke-Command -Quiet { & git co -fb $branch }
             Invoke-Command -Quiet { & git add . }
             Invoke-Command -Quiet { & git commit -m "$msg" }
-            Invoke-Command -Quiet { & git rebase origin/$branch }
-            Invoke-Command -Quiet { & git push origin $branch:$branch }
+            Invoke-Command -Quiet { & git rebase -srecursive -Xours origin/$branch }
+            Invoke-Command -Quiet { & git push origin ${branch}:${branch} }
         } finally {
             Pop-Location
         }
