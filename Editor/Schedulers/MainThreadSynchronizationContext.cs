38a73be917f7589d6d190d5e076e7713024d5d14
publishbot <publishbot@spoiledcat.net>
2020-10-22 11:12:37 +0000

    com.spoiledcat.threading v2.0.13+5a5bc9dd03

---
+++ Editor/Schedulers/MainThreadSynchronizationContext.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading;
+
+namespace SpoiledCat.Threading
+{
+	using Helpers;
+
+#if UNITY_EDITOR
+
+	using UnityEditor;
+
+	public class MainThreadSynchronizationContext: SynchronizationContext, IMainThreadSynchronizationContext
+	{
+		public MainThreadSynchronizationContext(CancellationToken token = default) { }
+
+		public void Schedule(Action action)
+		{
+			action.EnsureNotNull(nameof(action));
+			Post(act => ((Action)act)(), action);
+		}
+
+		public override void Post(SendOrPostCallback d, object state)
+		{
+			if (d == null)
+				return;
+
+			EditorApplication.delayCall += () => d(state);
+		}
+
+		public void Dispose() {}
+	}
+#else
+	public class MainThreadSynchronizationContext : ThreadSynchronizationContext, IMainThreadSynchronizationContext
+	{
+		public MainThreadSynchronizationContext(CancellationToken token = default) : base(token) {}
+
+		public void Schedule(Action action)
+		{
+			action.EnsureNotNull(nameof(action));
+			Post(act => ((Action)act)(), action);
+		}
+	}
+#endif
+}
