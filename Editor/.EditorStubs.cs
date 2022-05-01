c514c8eb7c02729e220de86860c40d7ca14b3fba
publishbot <publishbot@spoiledcat.net>
2022-05-01 11:28:44 +0000

    com.spoiledcat.environment v2.1.0

--- Editor/.EditorStubs.cs
+++ Editor/.EditorStubs.cs
@@ -1,6 +1,6 @@
 #if !UNITY_EDITOR
 
-// Copyright 2016-2020 Andreia Gaita
+// Copyright 2016-2022 Andreia Gaita
 //
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
