0dd733085c9096fdd71afaf4bd70e76a2b1de632
publishbot <publishbot@spoiledcat.net>
2022-07-11 14:37:06 +0000

    com.spoiledcat.utilities v2.1.4+020d809dbc

--- Editor/Version.cs
+++ Editor/Version.cs
@@ -11,8 +11,8 @@
 #pragma warning disable CA2243
 
 [assembly: System.Reflection.AssemblyVersionAttribute("2.1.0.0")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("2.1.2.7332")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.1.2+a41c9c380b")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("2.1.4.3330")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.1.4+020d809dbc")]
 #if NETSTANDARD || NETFRAMEWORK || NETCOREAPP
 [System.CodeDom.Compiler.GeneratedCode("Nerdbank.GitVersioning.Tasks","3.4.255.64262")]
 #endif
@@ -21,12 +21,12 @@
 #endif
 internal static partial class ThisAssembly {
     internal const string AssemblyConfiguration = "Release";
-    internal const string AssemblyFileVersion = "2.1.2.7332";
-    internal const string AssemblyInformationalVersion = "2.1.2+a41c9c380b";
+    internal const string AssemblyFileVersion = "2.1.4.3330";
+    internal const string AssemblyInformationalVersion = "2.1.4+020d809dbc";
     internal const string AssemblyName = "SpoiledCat.Unity.Utilities.Editor";
     internal const string AssemblyVersion = "2.1.0.0";
-    internal static readonly System.DateTime GitCommitDate = new System.DateTime(637931413880000000L, System.DateTimeKind.Utc);
-    internal const string GitCommitId = "a41c9c380b109d5ebe181b0e315558fc7a371e28";
+    internal static readonly System.DateTime GitCommitDate = new System.DateTime(637931446650000000L, System.DateTimeKind.Utc);
+    internal const string GitCommitId = "020d809dbc1674f5dcf04a076cf61acc468ac702";
     internal const bool IsPrerelease = false;
     internal const bool IsPublicRelease = false;
     internal const string PublicKey = "0024000004800000940000000602000000240000525341310004000001000100117d0dec0a3fe810510e005bd6a1cb5aca2af1a411346777d30fe1aa0e9fa786495ab06591d36124aacc8e0ff799a47233ca73de6d91935796e339b1564cfbfcdfc7aed3032449af2641359289721cd4354823e0b94779b7d81c2b531c775e7cb03472fe00acc0e325e48620683d4b6ee8df9b4ff39d45cd7de0747e6a0226bd";
