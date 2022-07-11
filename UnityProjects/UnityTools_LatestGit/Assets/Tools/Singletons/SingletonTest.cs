3e73e3fb3f4222820512c00f699a7ff5784dca81
Andreia Gaita <shana@spoiledcat.net>
2022-07-11 19:00:34 +0200

    Fix runtime codepath for the singleton

--- UnityProjects/UnityTools_LatestGit/Assets/Tools/Singletons/SingletonTest.cs
+++ UnityProjects/UnityTools_LatestGit/Assets/Tools/Singletons/SingletonTest.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SingletonTest : MonoBehaviour
 {
+	public Text text;
 	// Start is called before the first frame update
 	void Start()
 	{
 		var ins = SingletonPrefab.Instance;
 		Debug.Log($"The Default is {ins.theDefault}");
+		text.text = $"The Default in {ins.name} is {ins.theDefault}";
 	}
 }
