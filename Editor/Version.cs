02bae24e59bf36a0d78ea5d35c0ef56e343370cd
publishbot <publishbot@spoiledcat.net>
2024-01-16 14:15:59 +0000

    com.spoiledcat.threading.tests v2.2.6

--- Editor/Version.cs
+++ Editor/Version.cs
@@ -28,7 +28,7 @@ internal static partial class ThisAssembly {
     internal static readonly System.DateTime GitCommitDate = new System.DateTime(638410020240000000L, System.DateTimeKind.Utc);
     internal const string GitCommitId = "f555fce312c5402a567ea44622f7511b2f55d8d0";
     internal const bool IsPrerelease = false;
-    internal const bool IsPublicRelease = false;
+    internal const bool IsPublicRelease = true;
     internal const string PublicKey = "0024000004800000940000000602000000240000525341310004000001000100117d0dec0a3fe810510e005bd6a1cb5aca2af1a411346777d30fe1aa0e9fa786495ab06591d36124aacc8e0ff799a47233ca73de6d91935796e339b1564cfbfcdfc7aed3032449af2641359289721cd4354823e0b94779b7d81c2b531c775e7cb03472fe00acc0e325e48620683d4b6ee8df9b4ff39d45cd7de0747e6a0226bd";
     internal const string PublicKeyToken = "506395a6745f91e6";
     internal const string RootNamespace = "SpoiledCat.Unity.Threading";
