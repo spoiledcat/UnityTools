d7c70a943f80994600760e98997867abf57a8c49
publishbot <publishbot@spoiledcat.net>
2024-07-02 22:02:57 +0000

    com.spoiledcat.threading.tasks v2.2.7+f32e1f86a3

--- Editor/Version.cs
+++ Editor/Version.cs
@@ -11,8 +11,8 @@
 #pragma warning disable CA2243
 
 [assembly: System.Reflection.AssemblyVersionAttribute("2.2.0.0")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("2.2.6.22005")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.2.6+f555fce312")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("2.2.7.12019")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.2.7+f32e1f86a3")]
 #if NETSTANDARD || NETFRAMEWORK || NETCOREAPP
 [System.CodeDom.Compiler.GeneratedCode("Nerdbank.GitVersioning.Tasks","3.4.255.64262")]
 #endif
@@ -21,14 +21,14 @@
 #endif
 internal static partial class ThisAssembly {
     internal const string AssemblyConfiguration = "Release";
-    internal const string AssemblyFileVersion = "2.2.6.22005";
-    internal const string AssemblyInformationalVersion = "2.2.6+f555fce312";
+    internal const string AssemblyFileVersion = "2.2.7.12019";
+    internal const string AssemblyInformationalVersion = "2.2.7+f32e1f86a3";
     internal const string AssemblyName = "SpoiledCat.Unity.Threading.Tasks";
     internal const string AssemblyVersion = "2.2.0.0";
-    internal static readonly System.DateTime GitCommitDate = new System.DateTime(638410020240000000L, System.DateTimeKind.Utc);
-    internal const string GitCommitId = "f555fce312c5402a567ea44622f7511b2f55d8d0";
+    internal static readonly System.DateTime GitCommitDate = new System.DateTime(638555542110000000L, System.DateTimeKind.Utc);
+    internal const string GitCommitId = "f32e1f86a3dedcb6dc9fe68ed0d38feb315cea2e";
     internal const bool IsPrerelease = false;
-    internal const bool IsPublicRelease = true;
+    internal const bool IsPublicRelease = false;
     internal const string PublicKey = "0024000004800000940000000602000000240000525341310004000001000100117d0dec0a3fe810510e005bd6a1cb5aca2af1a411346777d30fe1aa0e9fa786495ab06591d36124aacc8e0ff799a47233ca73de6d91935796e339b1564cfbfcdfc7aed3032449af2641359289721cd4354823e0b94779b7d81c2b531c775e7cb03472fe00acc0e325e48620683d4b6ee8df9b4ff39d45cd7de0747e6a0226bd";
     internal const string PublicKeyToken = "506395a6745f91e6";
     internal const string RootNamespace = "SpoiledCat.Unity.Threading.Tasks";
